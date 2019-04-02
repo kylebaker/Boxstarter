a3708635115144a85bdf13fd430d87f472e90efd
Kyle Baker <10749890@uvu.edu>
2019-04-01 21:15:22 -0600

    more testing

--- main.ps1
+++ main.ps1
@@ -26,7 +26,7 @@ function executeScript {
 
 executeScript "FileExplorerSettings.ps1";
 executeScript "SystemConfiguration.ps1";
-executeScript "WSL.ps1";
+#executeScript "WSL.ps1";
 #executeScript "RemoveDefaultApps.ps1";
 #executeScript "Browsers.ps1";
 
