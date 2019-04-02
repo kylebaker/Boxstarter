7c7f29dbfea5ee350441f198fea38d0e8f7f8c66
Kyle Baker <10749890@uvu.edu>
2019-04-01 19:54:55 -0600

    fdas

--- scripts/WSL.ps1
+++ scripts/WSL.ps1
@@ -2,15 +2,13 @@
 
 choco install -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
 
-Invoke-Reboot
-
 #--- Ubuntu ---
 # TODO: Move this to choco install once --root is included in that package
-Invoke-WebRequest https://aka.ms/wsl-ubuntu-1804 -out ubuntu.zip -UseBasicParsing
-Expand-Archive .\ubuntu.zip
+#Invoke-WebRequest https://aka.ms/wsl-ubuntu-1804 -out ubuntu.zip -UseBasicParsing
+#Expand-Archive .\ubuntu.zip
 # run the distro once and have it install locally with root user, unset password
 
-RefreshEnv
-Ubuntu1804 install --root
-Ubuntu1804 run apt update
-Ubuntu1804 run apt upgrade -y
+#RefreshEnv
+#Ubuntu1804 install --root
+#Ubuntu1804 run apt update
+#Ubuntu1804 run apt upgrade -y
