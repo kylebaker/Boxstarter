f5e0ca0c310a41dbd7216233d2cf78c28d23c249
kylebaker <kylethomasbaker@gmail.com>
2019-04-02 19:23:59 -0600

    dfsa

--- main.ps1
+++ main.ps1
@@ -26,10 +26,10 @@ function executeScript {
 
 executeScript "FileExplorerSettings.ps1";
 executeScript "SystemConfiguration.ps1";
-executeScript "WSL.ps1";
-executeScript "RemoveDefaultApps.ps1";
+#executeScript "WSL.ps1";
+#executeScript "RemoveDefaultApps.ps1";
 executeScript "Browsers.ps1";
-executeScript "EssentialApps.ps1";
+#executeScript "EssentialApps.ps1";
 
 
 #--- reenabling critial items ---
