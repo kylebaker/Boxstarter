8568ac0361cd2dfc37e508ed6211009c74494765
kylebaker <kylethomasbaker@gmail.com>
2019-04-29 12:45:30 -0600

    typo stuff

--- scripts/internaldev.ps1
+++ scripts/internaldev.ps1
@@ -1,4 +1,4 @@
 # Taken directly from: https://github.com/Microsoft/windows-dev-box-setup-scripts/blob/master/scripts/Browsers.ps1
 #--- Browsers ---
-choco install -y vs2013 -s http://palm/choclatey
-choco install -y office2003 -s http://palm/choclatey
+choco install -y vs2013 -s http://palm/chocolatey
+choco install -y office2003 -s http://palm/chocolatey
