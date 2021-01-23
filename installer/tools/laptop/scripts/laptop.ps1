e99da0d72775233e1e52d51a9abea31999690d4f
kylebaker <kylethomasbaker@gmail.com>
2021-01-23 13:58:25 -0500

    testing choco packages

--- installer/tools/laptop/scripts/laptop.ps1
+++ installer/tools/laptop/scripts/laptop.ps1
@@ -6,9 +6,9 @@ choco install -y firefox
 choco install -y github-desktop
 choco install -y hwmonitor
 choco install -y k-litecodecpackfull 
-#choco install -y microsoft-windows-terminal #not working with fresh 1903 atm
+choco install -y microsoft-windows-terminal #not working with fresh 1903 atm
 choco install -y putty
-choco install -y nextcloud-client
+#choco install -y nextcloud-client
 choco install -y namebench
 choco install -y powertoys
 choco install -y signal
