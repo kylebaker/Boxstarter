a844d8d7664e79c4cd17024674c1906cb8a74815
kylebaker <kylethomasbaker@gmail.com>
2021-01-23 16:04:40 -0500

    Update CleanUp.ps1

--- installer/tools/scripts/CleanUp.ps1
+++ installer/tools/scripts/CleanUp.ps1
@@ -5,9 +5,9 @@ Enable-UAC
   # clean up the cache directory
   Remove-Item $cache -Recurse
 
-  # Remove shortcuts on desktop
-  Remove-Item C:\Users\*\Desktop\*lnk –Force
-
   # Remove desktop.ini files
   Get-ChildItem -Path (Join-Path ${Env:UserProfile} "Desktop") -Hidden -Filter "desktop.ini" -Force | foreach {$_.Delete()}
   Get-ChildItem -Path (Join-Path ${Env:Public} "Desktop") -Hidden -Filter "desktop.ini" -Force | foreach {$_.Delete()}
+
+  # Remove shortcuts on desktop
+  Remove-Item C:\Users\*\Desktop\*lnk –Force
